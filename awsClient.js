418de59af6b6469aaa24fc6c76a7efe6345d735c
Michael Meluso <melusom2@tcnj.edu>
2015-03-30 19:00:48 -0400

    Email validation
    Added logic to check if no emails are entered or if a value is not
    formatted like an email address

--- awsClient.js
+++ awsClient.js
@@ -8,8 +8,6 @@ var s3 = new AWS.S3();
 var dynamodb = new AWS.DynamoDB();
 var ses = new AWS.SES();
 
-var charset = "utf-8";
-
 // Creates a new bucket for a room
 exports.createBucket = function(roomID) {
     var name = 'tcnj-csc470-nodejs-' + roomID;
@@ -60,7 +58,7 @@ exports.sendEmail = function(sendTo, instructor) {
 
     var params = {
       Destination: { /* required */
-        ToAddresses: sendTo
+        BccAddresses: sendTo
       },
       Message: { /* required */
         Body: { /* required */
