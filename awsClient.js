638938eb9590d54341078790154778cd53836d9b
Brandon Gottlob <bgottlob1@gmail.com>
2015-03-30 17:36:05 -0400

    Developed callback to ensure that new room IDs are unique

--- awsClient.js
+++ awsClient.js
@@ -1,4 +1,5 @@
-var AWS = require('aws-sdk');
+var AWS = require('aws-sdk'),
+    wait = require("wait.for");
 
 // Set the region for the AWS services - required for DynamoDB
 AWS.config.update({region: "us-east-1"});
@@ -22,6 +23,64 @@ exports.createBucket = function(roomID) {
     s3.createBucket(params, function(err, data) {
         if (err) console.log(err, err.stack); // an error occurred
         else     console.log(data);           // successful response
+    });
+}
+
+/*
+ * Queries DynamoDB to see whether the room ID given in the parameter has been used already
+ * If the room ID is unique, then a true value is passed into the parameter callback
+ * If the room ID is not unique, then a false value is passed into the parameter callback
+ */
+exports.testRoomID = function(roomID, callback) {
+
+    var result;
+
+    var params = {
+        Key: {
+            RoomID: { S: roomID }
+        },
+        TableName: "Room"
+    };
+
+    dynamodb.getItem(params, function(err, data){
+        if (err){ 
+            console.log(err, err.stack);
+            callback(false);
+        }
+        else {
+            callback(typeof data.Item === "undefined");
+        }
+    });
+
+    console.log("Got here");
+    return result;
+
+}
+
+exports.getUniqueRoomID = function() {
+
+    var roomID = this.randID();
+
+    var params = {
+        Key: {
+            RoomID: { S: roomID }
+        },
+        TableName: "Room"
+    };
+
+    console.log("getting item");
+
+    dynamodb.getItem(params, function(err, data){
+        if (err) console.log(err, err.stack);
+        else {
+            console.log("Finished dynamodb call for " + roomID);
+            if(typeof data.Item === "undefined") {
+                console.log("Returning unique ID");
+                return roomID;
+            }
+            else
+                this.getUniqueRoomID();
+        }
     });
 }
 
