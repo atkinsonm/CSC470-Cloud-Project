c4060356f05ae45ba1289d19ae9a91274bdfdae5
Vitor <vm17sun@gmail.com>
2015-03-31 00:10:04 -0400

    Editing the createroom script to send the file content to the node server.

--- newroom/createroom.js
+++ newroom/createroom.js
@@ -1,3 +1,5 @@
+var inputData = {};
+
 $(document).ready(function() {
 
 	var socketIOFileName = document.documentURI + "socket.io/socket.io.js";
@@ -17,14 +19,33 @@ $(document).ready(function() {
 			}
 
 			// A JS object that is sent to the server - packages the input data
-			var inputData = {
+			inputData = {
 				instructorName: $('#instructornameinput').val(),
 				roomName: $('#roomnameinput').val(),
 				emails: emails
 			};
 
+			// getting the file object using jquery.
+			var file = $('#presentationfile')[0].files[0];
+
+			// preparing the file reader and events.
+			var FReader = new FileReader();
+
+			FReader.onloadend = function(evt){
+				inputData.file = {
+					'name' : file.name.split('.')[0],
+					'extension' : file.name.split('.')[1],
+					'data' : evt.target.result
+				}
+				
+				console.log(inputData);
+			}
+
+			// reading the file.
+			FReader.readAsBinaryString(file);	
+
 			// Emit a socket event to the server and send the input data
-			socket.emit('create-room', inputData);
+			//socket.emit('create-room', inputData);
 		});
 	});
 });
